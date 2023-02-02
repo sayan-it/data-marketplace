2d9bec8e9ad4b7bb484a460b7126c8122bdee533
Roy <sayroy@deloitte.com>
2023-02-02 15:45:55 +0530

    visual changes and api reformation added

--- DMA frontend/src/components/DataFilterForm.js
+++ DMA frontend/src/components/DataFilterForm.js
@@ -28,82 +28,84 @@ export default function DataFilterForm() {
 
   const handleSearch = (queryText) => {
     axios
-      .get(`http://localhost:3200/elastic?text=${queryText}`)
+      .get(`http://localhost:8000/search/data/${queryText}`)
       .then((result) => {
-        setRecords(result.data.records);
+        setRecords(result.data.results);
       })
       .catch((err) => console.log(err.response.data.json()));
   };
 
   return (
-    <div className="row">
-      <div className="col-md-3">
-        <h3>Select Data</h3>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3">
+          <h3>Select Data</h3>
 
-        {table.map((ele, index) => {
-          return (
-            <div className="table-list-item">
-              <input
-                type="checkbox"
-                id={`custom-checkbox-${index}`}
-                name={ele}
-                value={ele}
-                checked={checkedState[index]}
-                onChange={() => handleOnChange(index)}
-              />
-              <label htmlFor={`custom-checkbox-${index}`}>{ele}</label>
-            </div>
-          );
-        })}
-      </div>
-      <div className="col-md-9">
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            name="searchData"
-            value={search}
-            placeholder="Search Data"
-            onChange={(e) => searchhData(e.target.value)}
-          />
-          <button
-            className="btn btn-success"
-            onClick={() => handleSearch(search)}
-          >
-            Search
-          </button>
+          {table.map((ele, index) => {
+            return (
+              <div className="table-list-item">
+                <input
+                  type="checkbox"
+                  id={`custom-checkbox-${index}`}
+                  name={ele}
+                  value={ele}
+                  checked={checkedState[index]}
+                  onChange={() => handleOnChange(index)}
+                />
+                <label htmlFor={`custom-checkbox-${index}`}>{ele}</label>
+              </div>
+            );
+          })}
         </div>
-        <div>
-          {foundrecords.length > 0 ? (
-            <div className="table-responsive">
-              <table class="table table-striped table-responsive">
-                <thead>
-                  <tr>
-                    {Object.keys(foundrecords[0]).map((ele, ind) => (
-                      <th>{ele}</th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {foundrecords.map((ele, ind) => {
-                    return (
-                      <tr>
-                        {Object.values(ele).map((data, id) => {
-                          return typeof data === "boolean" ? (
-                            <td>{JSON.stringify(data)}</td>
-                          ) : (
-                            <td>{data}</td>
-                          );
-                        })}
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          ) : (
-            ""
-          )}
+        <div className="col-md-9">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              name="searchData"
+              value={search}
+              placeholder="Search Data"
+              onChange={(e) => searchhData(e.target.value)}
+            />
+            <button
+              className="btn btn-success"
+              onClick={() => handleSearch(search)}
+            >
+              Search
+            </button>
+          </div>
+          <div>
+            {foundrecords.length > 0 ? (
+              <div className="table-responsive">
+                <table class="table table-striped table-responsive">
+                  <thead>
+                    <tr>
+                      {Object.keys(foundrecords[0]).map((ele, ind) => (
+                        <th>{ele}</th>
+                      ))}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {foundrecords.map((ele, ind) => {
+                      return (
+                        <tr>
+                          {Object.values(ele).map((data, id) => {
+                            return typeof data === "boolean" ? (
+                              <td>{JSON.stringify(data)}</td>
+                            ) : (
+                              <td>{data}</td>
+                            );
+                          })}
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
       </div>
     </div>
